196f3bd37a67aa55d38c614aec9491da962a6e81
kader chriette <kader chriette>
2016-10-27 13:01:30 -0500

    angular project

---
+++ js/controlers/appCtrl.js
@@ -0,0 +1,8 @@
+'use strict'
+angular.module("appName")
+    .controller("appCtrl",["$scope",function($scope){
+        $scope.header="hello world";
+        $scope.test=function(){
+            $scope.header="oh look it change ";
+        }
+    }]);
---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+ app.use(express.static(__dirname +'/'));
+app.get('/', function (req, res) {
+  res.redirect('/views/index.html');
+});
+ 
+app.listen(3000);
